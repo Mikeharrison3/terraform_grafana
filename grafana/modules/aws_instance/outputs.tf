7131e511d02cac1546c2960e29d16b871ce45bda
Michael <michaelaharrison@gmail.com>
2021-07-08 21:08:47 -0400

    Updated the main module to output the loadbalancer ip and server public ip.

--- grafana/modules/aws_instance/outputs.tf
+++ grafana/modules/aws_instance/outputs.tf
@@ -1,5 +1,9 @@
 output "server_public_ip" {
     value = aws_instance.grafana-web-server-instance.public_ip
+
+    depends_on = [
+      aws_instance.grafana-web-server-instance
+    ]
 }
 
 output "loadbalancer_ip" {
---
+++ grafana/outputs.tf
@@ -0,0 +1,9 @@
+output "server_public_ip" {
+    value = module.instances.server_public_ip
+
+  
+}
+
+output "loadbalancer_ip" {
+    value = module.instances.loadbalancer_ip
+}
